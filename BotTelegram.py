57d7281a933cb7adb69a37d17d68a48789ae0baf
Matteo Rossi <45334256+reddi32@users.noreply.github.com>
2023-01-31 11:06:43 +0100

    Add files via upload

--- BotTelegram.py
+++ BotTelegram.py
@@ -2,30 +2,35 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-def send_message(chat_id, text):
-    # funzione per inviare un messaggio a Telegram
-    url = "https://api.telegram.org/bot{}/sendMessage".format(TELEGRAM_BOT_TOKEN)
-    data = {"chat_id": chat_id, "text": text}
-    response = requests.post(url, data=data)
+def send_message(chat_id, text, bot_token):
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={text}"
+    requests.get(url)
 
-def check_for_new_article(site_url, article_title_selector):
-    # funzione per verificare se ci sono nuovi articoli sul sito
-    page = requests.get(site_url)
+def check_new_article(url, bot_token, chat_id):
+    # Estrarre il contenuto del sito web
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
-    article_title = soup.select_one(article_title_selector).text
+    articles = soup.find_all("article")
+    # Verificare se ci sono nuovi articoli
+    if len(articles) > 0:
+        new_article = articles[0]
+        title = new_article.find("h2").text
+        # Verificare se l'articolo è già stato notificato
+        if title not in notified_articles:
+            notified_articles.append(title)
+            # Inviare il messaggio su Telegram
+            text = f"Nuovo articolo pubblicato: {title}"
+            send_message(chat_id, text, bot_token)
 
-    with open("notified_articles.txt", "a+") as f:
-        f.seek(0)
-        notified_articles = f.readlines()
-        notified_articles = [x.strip() for x in notified_articles]
-
-        if article_title not in notified_articles:
-            send_message(TELEGRAM_CHAT_ID, "Nuovo articolo pubblicato: " + article_title)
-            f.write(article_title + "\n")
-
-TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-TELEGRAM_CHAT_ID = "163919711"
-SITE_URL = "https://www.turismoroma.it/"
-ARTICLE_TITLE_SELECTOR = "#latest-news h2 a"
-
-check_for_new_article(SITE_URL, ARTICLE_TITLE_SELECTOR)
+# Impostare il URL del sito web
+url = "https://www.turismoroma.it/"
+# Sostituire con il token del bot
+bot_token = "6165661441:AAGUdYk4oMsfuKV3hp5LDaeVZzB0VOnxdZQ"
+# Sostituire con il chat ID
+chat_id = "163919711"
+# Mantenere un elenco degli articoli notificati
+notified_articles = []
+# Verificare se ci sono nuovi articoli ogni 10 minuti
+while True:
+    check_new_article(url, bot_token, chat_id)
+    time.sleep(600)
